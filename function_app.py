3419bf1e7a7ad858eaee513b6dd064621e23d9cd
tech dor <tech.aharoni@gmail.com>
2024-04-30 14:59:03 +0300

    adding libraries

--- function_app.py
+++ function_app.py
@@ -1,10 +1,15 @@
 import azure.functions as func
 import logging
-
+import os #in order to get parameters values from azure function app enviroment vartiable - sql password for example 
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient # in order to use azure container storage
+from PyPDF2 import PdfReader,PdfWriter  # in order to read and write  pdf file 
+import io # in order to download pdf to memory and write into memory without disk permission needed 
+import json # in order to use json 
+import pyodbc #for sql connections 
 app = func.FunctionApp()
 
 @app.service_bus_queue_trigger(arg_name="azservicebus", queue_name="split",
                                connection="medicalanalysis_SERVICEBUS") 
 def sb_split_process(azservicebus: func.ServiceBusMessage):
-    logging.info('Python ServiceBus Queue trigger processed a message: %s',
-                azservicebus.get_body().decode('utf-8'))
+    message_data = azservicebus.get_body().decode('utf-8')
+    logging.info('Received messageesds: %s', message_data)
